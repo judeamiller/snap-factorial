0c3fd1e3ebda39c804d270f2fb64cff1369391cb
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-23 09:12:04 -0600

    Complete Factorial Code.

---
+++ snap-factiorial.php
@@ -0,0 +1,27 @@
+<?php
+
+//write a function that calculates the factorial of n, where:
+//n! = n * (n-1) *  (n - 2) * ... *2 * 1
+//
+// 0! = 1
+//throw exception if n< 0 so do notn allow negatives
+// throw an exception if n> 20 so 21 and above are overflow in type
+
+
+
+function factorial(int $n): int {
+	if($n < 0) {
+		throw(new \RangeException("no negatives"));
+	}
+	if ($n > 20){
+		throw(new \RangeException("numbers greater than 21"));
+	}
+
+	$answer = 1;  // default to 0!=1
+	for($i = 1; $i <= $n; $i++) {
+		$answer = $answer * $i;
+	}
+	return $answer;
+}
+
+echo factorial(6) . PHP_EOL;
